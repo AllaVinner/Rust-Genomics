72cc12560d0aad652db83e4701d704acfa087f3a
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-18 21:37:58 +0200

    Working initial CLI

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    NumRecords{file_path: String},
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::NumRecords {file_path} => {
+            println!("Input file is: {:?}", file_path)
+        }
+    }
+}
