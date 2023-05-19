8dce0156acd4f65af6312842179b321de781fd95
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-19 20:55:07 +0200

    Remove CLI

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,5 +1,19 @@
+
+
+
+
+
+const STOP_CODONS: [[&DNABase; 3]; 3] = [[&DNABase::T, &DNABase::A, &DNABase::A],
+                                        [&DNABase::T, &DNABase::A, &DNABase::G],
+                                        [&DNABase::T, &DNABase::G, &DNABase::A]];
+                     
+
+const START_CODONS: [[&DNABase; 3]; 1] = [[&DNABase::A, &DNABase::T, &DNABase::G]];
+
+const CODON_LENGTH: usize = 3;
+
 //TODO: Move this at some point
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum DNABase {
     A,
     C,
@@ -52,6 +66,34 @@ pub fn parse_fasta(fasta_input: &str) -> Vec<FastaSequence>{
         .collect()
 }
 
+fn get_orfs(seq: &Vec<DNABase>, offset: usize) -> Vec<&[DNABase]> { 
+    let (_, orfs) = seq
+        .iter()
+        .skip(offset)
+        .array_chunks::<CODON_LENGTH>()
+        .enumerate()
+        .fold((Vec::<usize>::new(), Vec::<&[DNABase]>::new()), 
+            |(mut buffer, mut orfs), (codon_i, codon)| {
+            
+                if START_CODONS.contains(&codon) {
+                    buffer.push(codon_i);
+                }
+                
+                if STOP_CODONS.contains(&codon) {
+                    for start_i in &buffer{
+                        orfs.push(&seq[CODON_LENGTH*start_i..CODON_LENGTH*(codon_i+1)]);
+                    }
+                    buffer.clear();
+                }
+                return (buffer, orfs)
+            }
+            );
+    return orfs
+}
+
+
+
+
 pub fn num_records(fasta_input: &str) -> i32 {
     fasta_input.split(">").count() as i32 - 1
 }
@@ -59,6 +101,13 @@ pub fn num_records(fasta_input: &str) -> i32 {
 pub fn record_lengths(fasta_input: &str) -> Vec<(String, usize)> {
     parse_fasta(fasta_input).iter().map(|f| (f.identifier.clone(), f.data.len())).collect()
 }
+
+
+pub fn record_max_orf(fasta_input: &str) -> Vec<(String, usize)> {
+    parse_fasta(fasta_input).iter().map(|f| (f.identifier.clone(), f.data.len())).collect()
+}
+
+
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,9 @@
-use clap::{Parser, Subcommand};
+#![feature(iter_array_chunks)]
+#![feature(array_chunks)]
+
 mod fasta;
 
-use std::fs;
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-#[command(propagate_version = true)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Adds files to myapp
-    NumRecords{file_path: String},
-    RecordLengths{file_path: String},
-}
 
 fn main() {
-    let cli = Cli::parse();
 
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Commands::NumRecords {file_path} => {
-            let file = fs::read_to_string(file_path).unwrap().replace("\r", "");
-            let num_records = fasta::num_records(&file);
-            println!("Num records was {:?}", num_records);
-        }
-        Commands::RecordLengths {file_path} => {
-            let file = fs::read_to_string(file_path).unwrap().replace("\r", "");
-            let lengths = fasta::record_lengths(&file);
-            println!("Num records was {:?}", lengths);
-        }
-    }
 }
