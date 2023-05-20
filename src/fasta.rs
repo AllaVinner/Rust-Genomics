04fd00f000b8f6d0bb33f0d9e49e2e85bf42c208
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-20 11:45:24 +0200

    Actually working materialized fasta

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,5 +1,15 @@
 
 
+const STOP_CODONS: [[DNABase; 3]; 3] = [[DNABase::T, DNABase::A, DNABase::A],
+                                        [DNABase::T, DNABase::A, DNABase::G],
+                                        [DNABase::T, DNABase::G, DNABase::A]];                  
+
+const START_CODONS: [[DNABase; 3]; 1] = [[DNABase::A, DNABase::T, DNABase::G]];
+const CODON_LENGTH: usize = 3;
+
+type DNASequence = Vec<DNABase>;
+type ORF<'a> = &'a [DNABase];
+
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum DNABase {
     A,
@@ -8,7 +18,6 @@ pub enum DNABase {
     T
 }
 
-type DNASequence = Vec<DNABase>;
 pub struct Read {
     id: String,
     desc: String,
@@ -33,16 +42,20 @@ fn char_to_dna_base(c: char) -> Option<DNABase> {
 }
 
 pub fn str_to_seq(input: &str) -> DNASequence {
-    input.chars().map(char_to_dna_base).collect()
+    input.chars().filter_map(char_to_dna_base).collect()
 }
 
-pub fn str_to_read(input: str) -> Read{
-    let (a, b) =  input.split_once(">").unwrap();
-    if a == "" {
-        input = b;
-    }
+pub fn str_to_read(input: &str) -> Read{
+    let start = match input.split_once(">") {
+        None => input,
+        Some((a,b)) => if a == "" {
+            b
+        } else {
+            input
+        },
+    };
 
-    let (header, data) = input.split_once("\n").unwrap();
+    let (header, data) = start.split_once("\n").unwrap();
     let (id, desc) = header.split_once(" ").unwrap();
     Read {
         id: id.to_string(),
@@ -51,13 +64,43 @@ pub fn str_to_read(input: str) -> Read{
     }
 }
 
+pub fn str_to_fasta(input: &str) -> Vec<Read> {
+    input.split(">").skip(1).map(str_to_read).collect()
+}
+
+pub fn seq_to_orfs<'a>(seq: &'a DNASequence) -> Vec<ORF<'a>> {
+    seq.array_chunks::<CODON_LENGTH>()
+   .enumerate()
+   .fold((Vec::<usize>::new(), Vec::<ORF>::new()), 
+         |(mut buffer, mut orfs), (codon_i, codon)| {
+         
+             if START_CODONS.contains(codon) {
+                 buffer.push(codon_i);
+             }
+             
+             if STOP_CODONS.contains(codon) {
+                 for start_i in &buffer{
+                    orfs.push(&seq[CODON_LENGTH*start_i..CODON_LENGTH*(codon_i+1)]);
+                 }
+                 buffer.clear();
+             }
+             return (buffer, orfs)
+         }
+         ).1
+}
+
+
+
+   
+         
+
 pub fn main(fasta: &str) {
     let c = str_to_fasta(fasta).len();
     println!("Number of records: {c}");
 
-    let (longest_read, read_len) = str_to_virtual_fasta(fasta)
+    let (longest_read, read_len) = str_to_fasta(fasta)
         .iter()
-        .map(|read| (read.id, read.seq.len()))
+        .map(|read| (read.id.clone(), read.seq.len()))
         .reduce(|(max_id, max_len), (id, len)| {
             if len > max_len {
                 return (id, len)
@@ -65,7 +108,7 @@ pub fn main(fasta: &str) {
             return (max_id, max_len);
         }).unwrap();
     println!("Longest read: {longest_read}, with len {read_len}.");
-    
+
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![feature(iter_array_chunks)]
 #![feature(array_chunks)]
 use std::fs;
-mod virtual_fasta;
+mod fasta;
 
 
 fn main() {
     let file_path = "./data/dna-example.fasta";
     let file = fs::read_to_string(file_path).unwrap().replace("\r", "");
-    virtual_fasta::main(&file);
+    fasta::main(&file);
 }
