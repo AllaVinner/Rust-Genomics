d59c1051eb6f26c561bdff8a5e995ccc3b347a94
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-20 10:55:57 +0200

    Working nested iterator for fasta files

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,18 +1,7 @@
+use std::str::{Chars, Split};
+use std::iter::{FilterMap, Map, Skip};
 
 
-
-
-
-const STOP_CODONS: [[&DNABase; 3]; 3] = [[&DNABase::T, &DNABase::A, &DNABase::A],
-                                        [&DNABase::T, &DNABase::A, &DNABase::G],
-                                        [&DNABase::T, &DNABase::G, &DNABase::A]];
-                     
-
-const START_CODONS: [[&DNABase; 3]; 1] = [[&DNABase::A, &DNABase::T, &DNABase::G]];
-
-const CODON_LENGTH: usize = 3;
-
-//TODO: Move this at some point
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum DNABase {
     A,
@@ -21,94 +10,57 @@ pub enum DNABase {
     T
 }
 
-#[derive(Debug)]
-pub struct FastaSequence {
-    identifier: String,
-    description: String,
-    data: Vec<DNABase>
-}
 
-
-pub fn parse_fasta_sequence(input: &str) -> FastaSequence{
-
-    // Assumptions
-    // Has one new line
-    let (header, str_data) = input.split_once("\n").unwrap();
-    let (identifier, description) = if header.contains(" ") {
-        header.split_once(" ").unwrap()
-    } else {
-        (header, "")
-    };
-
-    
-    let mut data: Vec<DNABase> = str_data
-        .trim_end()
-        .chars().filter_map(|c| match c {
-            'A' | 'a' => Some(DNABase::A),
-            'C' | 'c' => Some(DNABase::C),
-            'G' | 'g' => Some(DNABase::G),
-            'T' | 't' => Some(DNABase::T),
-            '\n' => None,
-            _ => panic!("Data contains non actg data."),
-        }).collect();
-
-    FastaSequence{
-        identifier: identifier.to_string(),
-        description: description.to_string(),
-        data: data
+fn char_to_dna_base(c: char) -> Option<DNABase> {
+    match c {
+        'A' | 'a' => Some(DNABase::A),
+        'C' | 'c' => Some(DNABase::C),
+        'G' | 'g' => Some(DNABase::G),
+        'T' | 't' => Some(DNABase::T),
+        '\n' => None,
+        other => panic!("Got unexpected caracter in raw data. {:?}", other)
     }
 }
 
-pub fn parse_fasta(fasta_input: &str) -> Vec<FastaSequence>{
-    fasta_input.split(">")
-        .skip_while(|seq| seq == &"")
-        .map(|seq| parse_fasta_sequence(seq))
-        .collect()
+pub type VirtualSequence<'a> = FilterMap<Chars<'a>, fn(char) -> Option<DNABase>>;
+pub struct VirtualRead<'a> {
+    id: &'a str,
+    desc: & 'a str,
+    seq: VirtualSequence<'a>
 }
 
-fn get_orfs(seq: &Vec<DNABase>, offset: usize) -> Vec<&[DNABase]> { 
-    let (_, orfs) = seq
-        .iter()
-        .skip(offset)
-        .array_chunks::<CODON_LENGTH>()
-        .enumerate()
-        .fold((Vec::<usize>::new(), Vec::<&[DNABase]>::new()), 
-            |(mut buffer, mut orfs), (codon_i, codon)| {
-            
-                if START_CODONS.contains(&codon) {
-                    buffer.push(codon_i);
-                }
-                
-                if STOP_CODONS.contains(&codon) {
-                    for start_i in &buffer{
-                        orfs.push(&seq[CODON_LENGTH*start_i..CODON_LENGTH*(codon_i+1)]);
-                    }
-                    buffer.clear();
-                }
-                return (buffer, orfs)
+pub fn str_to_virtual_seq<'a>(input: &'a str) -> VirtualSequence<'a> {
+    input.chars().filter_map(char_to_dna_base)
+}
+
+pub fn str_to_virtual_read<'a>(input: &'a str) -> VirtualRead<'a> {
+    let (header, data) = input.split_once("\n").unwrap();
+    let (id, desc) = header.split_once(" ").unwrap();
+    VirtualRead {
+        id: id,
+        desc: desc,
+        seq: str_to_virtual_seq(data)
+    }
+}
+
+
+pub type VirtualFasta<'a> = Map<Skip<Split<'a, &'a str>>, fn(&'a str) -> VirtualRead<'a>>;
+pub fn str_to_virtual_fasta<'a>(input: &'a str) -> VirtualFasta<'a> {
+    input.split(">").skip(1).map(str_to_virtual_read)
+}
+
+
+pub fn main(fasta: &str) {
+    let c = str_to_virtual_fasta(fasta).count();
+    println!("Number of records: {c}");
+    let (longest_read, read_len) = str_to_virtual_fasta(fasta)
+        .map(|read| (read.id, read.seq.count()))
+        .reduce(|(max_id, max_len), (id, len)| {
+            if len > max_len {
+                return (id, len)
             }
-            );
-    return orfs
+            return (max_id, max_len);
+        }).unwrap();
+    println!("Longest read: {longest_read}, with len {read_len}.");
+    
 }
-
-
-
-
-pub fn num_records(fasta_input: &str) -> i32 {
-    fasta_input.split(">").count() as i32 - 1
-}
-
-pub fn record_lengths(fasta_input: &str) -> Vec<(String, usize)> {
-    parse_fasta(fasta_input).iter().map(|f| (f.identifier.clone(), f.data.len())).collect()
-}
-
-
-pub fn record_max_orf(fasta_input: &str) -> Vec<(String, usize)> {
-    parse_fasta(fasta_input).iter().map(|f| (f.identifier.clone(), f.data.len())).collect()
-}
-
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![feature(iter_array_chunks)]
 #![feature(array_chunks)]
-
+use std::fs;
 mod fasta;
 
 
 fn main() {
-
+    let file_path = "./data/dna-example.fasta";
+    let file = fs::read_to_string(file_path).unwrap().replace("\r", "");
+    fasta::main(&file);
 }
