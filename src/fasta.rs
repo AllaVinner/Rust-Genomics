1f8c63f119c3ca6c1d71da45351af956967eaec8
AllaVinner <joel.w.ottosson@gmail.com>
2023-05-20 11:55:16 +0200

    ADded Orf sampler

--- src/fasta.rs
+++ src/fasta.rs
@@ -1,10 +1,10 @@
 
 
-const STOP_CODONS: [[DNABase; 3]; 3] = [[DNABase::T, DNABase::A, DNABase::A],
-                                        [DNABase::T, DNABase::A, DNABase::G],
-                                        [DNABase::T, DNABase::G, DNABase::A]];                  
+const STOP_CODONS: [[&DNABase; 3]; 3] = [[&DNABase::T, &DNABase::A, &DNABase::A],
+                                        [&DNABase::T, &DNABase::A, &DNABase::G],
+                                        [&DNABase::T, &DNABase::G, &DNABase::A]];                  
 
-const START_CODONS: [[DNABase; 3]; 1] = [[DNABase::A, DNABase::T, DNABase::G]];
+const START_CODONS: [[&DNABase; 3]; 1] = [[&DNABase::A, &DNABase::T, &DNABase::G]];
 const CODON_LENGTH: usize = 3;
 
 type DNASequence = Vec<DNABase>;
@@ -68,25 +68,27 @@ pub fn str_to_fasta(input: &str) -> Vec<Read> {
     input.split(">").skip(1).map(str_to_read).collect()
 }
 
-pub fn seq_to_orfs<'a>(seq: &'a DNASequence) -> Vec<ORF<'a>> {
-    seq.array_chunks::<CODON_LENGTH>()
-   .enumerate()
-   .fold((Vec::<usize>::new(), Vec::<ORF>::new()), 
-         |(mut buffer, mut orfs), (codon_i, codon)| {
-         
-             if START_CODONS.contains(codon) {
-                 buffer.push(codon_i);
-             }
-             
-             if STOP_CODONS.contains(codon) {
-                 for start_i in &buffer{
-                    orfs.push(&seq[CODON_LENGTH*start_i..CODON_LENGTH*(codon_i+1)]);
-                 }
-                 buffer.clear();
-             }
-             return (buffer, orfs)
-         }
-         ).1
+pub fn seq_to_orfs<'a>(seq: &'a DNASequence, offset: usize) -> Vec<ORF<'a>> {
+    seq.iter()
+        .skip(offset)
+        .array_chunks::<CODON_LENGTH>()
+        .enumerate()
+        .fold((Vec::<usize>::new(), Vec::<ORF>::new()), 
+            |(mut buffer, mut orfs), (codon_i, codon)| {
+            
+                if START_CODONS.contains(&codon) {
+                    buffer.push(codon_i);
+                }
+                
+                if STOP_CODONS.contains(&codon) {
+                    for start_i in &buffer{
+                        orfs.push(&seq[CODON_LENGTH*start_i..CODON_LENGTH*(codon_i+1)]);
+                    }
+                    buffer.clear();
+                }
+                return (buffer, orfs)
+            }
+        ).1
 }
 
 
@@ -108,6 +110,20 @@ pub fn main(fasta: &str) {
             return (max_id, max_len);
         }).unwrap();
     println!("Longest read: {longest_read}, with len {read_len}.");
+    
+    let (longest_ord_read, orf_len) = str_to_fasta(fasta)
+        .iter()
+        .map(|read| (read.id.clone(), seq_to_orfs(&read.seq,2)))
+        .map(|(id, orfs)| (id, orfs.iter().map(|orf| orf.len()).max().unwrap_or(0)))
+        .reduce(|(max_id, max_len), (id, len)| {
+            if len > max_len {
+                return (id, len)
+            }
+            return (max_id, max_len);
+        }).unwrap();
+    println!("Longest orf read: {longest_ord_read}, with len {orf_len}.");
+    
+    
 
 }
 
